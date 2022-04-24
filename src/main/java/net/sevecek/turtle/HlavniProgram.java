c06af809b7d96cc45a4c0a2154cd006de6320576
andrea <a.rosenbreierova@gmail.com>
2022-04-24 16:31:09 +0200

    Další dva obrazce - kolečko a sluníčko

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -4,8 +4,9 @@ import net.sevecek.turtle.engine.*;
 
 public class HlavniProgram {
     Turtle zofka = new Turtle();
-    public void nakresliPrasatko(){
-        zofka.setLocation(110,110);
+
+    public void nakresliPrasatko() {
+        zofka.setLocation(110, 200);
         zofka.move(100);
         zofka.turnLeft(135);
         zofka.move(70);
@@ -38,20 +39,46 @@ public class HlavniProgram {
         zofka.move(150);
         zofka.penDown();
     }
-    public void nakreliObrazec1(){
-zofka.setLocation(400, 200);
+
+    public void nakreliObrazec1() {
+        zofka.setLocation(350, 100);
         for (int i = 0; i < 8; i++) {
-            zofka.move(80);
+            zofka.move(50);
             zofka.turnRight(45);
         }
+    }
+    public void nakresliObrazec2() {
+        zofka.setLocation(550, 100);
+        for (int i = 0; i < 72; i++) {
+            zofka.move(6);
+            zofka.turnRight(5);
+        }
+    }
+    public void nakresliObrazec3() {
+        zofka.setLocation(750, 100);
+        for (int i = 0; i < 72; i++) {
+            zofka.move(5);
+            zofka.turnRight(2.5);
+            if(i % 6 == 0) {
+                zofka.turnLeft(90);
+                zofka.move(30);
+                zofka.turnRight(180);
+                zofka.move(30);
+                zofka.turnLeft(90);
+            }
+            zofka.turnRight(2.5);
 
+        }
     }
 
 
 
     public void main(String[] args) {
-        //nakresliPrasatko();
+        nakresliPrasatko();
         nakreliObrazec1();
+        nakresliObrazec2();
+        nakresliObrazec3();
+
 
 
     }
